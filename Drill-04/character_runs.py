babd879f94f8d7b4405f479a8904e461ab94dc98
HyunSukKang(C106-PC04) <khsfashi@naver.com>
2019-10-02 18:39:55 +0900

    드릴04 완성
    drill 04 character left side/ right side move 구현

---
+++ Drill-04/character_runs.py
@@ -0,0 +1,35 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+x = 0
+direct = 0
+frame = 0
+while True:
+    if direct == 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += 5
+        delay(0.05)
+        get_events()
+        if x >= 800:
+            direct = 1
+    elif direct == 1:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+        get_events()
+        if x <= 0:
+            direct = 0
+
+
+close_canvas()
+
