d7979e746264fbb76ecce4b797ef22859de2215a
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-07 19:33:54 +0900

    Drill-05 최종완성
    2018180002 강현석 Drill-05 완료 커밋 횟수 : 4회
    https://youtu.be/mBLDlfhgIYw

--- Drill-05/line.py
+++ Drill-05/line.py
@@ -8,27 +8,41 @@ def handle_events():
     global ch_x, ch_y
     global turn
     global start
+    global i
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
             x, y = event.x, KPU_HEIGHT - 1 - event.y
+            start = 0
+            i = 0
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
         if event.type == SDL_MOUSEBUTTONDOWN:
+            start = 0
             x, y = event.x, KPU_HEIGHT - 1 - event.y
             if ch_x > x:
                 turn = 0
             elif ch_x < x:
                 turn = 1
-            for i in range(0, 100 + 1, 2):
-                t = 1 / 100
-                ch_x = (1-t) * ch_x + t * x
-                ch_y = (1-t) * ch_y + t * y
+            start = 1
     pass
 
+def character_moving(p1, p2):
+    global ch_x, ch_y
+    global i
+    global start
+
+    t = i / 100
+    ch_x = (1 - t) * p1[0] + t * p2[0]
+    ch_y = (1 - t) * p1[1] + t * p2[1]
+    delay(0.01)
+    if i >= 100:
+        start = 0
+        i = 0
+pass
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -40,16 +54,21 @@ x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 ch_x, ch_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
 turn = 1
+i = 0
+start = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    mouse.draw(x, y)
+    mouse.draw(x + 17, y - 25)
     character.clip_draw(frame * 100, 100 * turn, 100, 100, ch_x, ch_y)
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
+    if start == 1:
+        i += 1
+        character_moving((ch_x, ch_y), (x, y))
+
 
 close_canvas()
