b28c5202747d5c1dfef2ca6ff39d8dbf3ddbc3e6
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-07 18:48:42 +0900

    좌우 추가
    좌우 방향 추가

--- Drill-05/line.py
+++ Drill-05/line.py
@@ -2,10 +2,11 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-
 def handle_events():
     global running
     global x, y
+    global ch_x, ch_y
+    global turn
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -14,6 +15,15 @@ def handle_events():
             x, y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            if ch_x > x:
+                turn = 0
+            elif ch_x < x:
+                turn = 1
+
+
 
     pass
 
@@ -25,14 +35,16 @@ mouse = load_image('hand_arrow.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+ch_x, ch_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
+turn = 1
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     mouse.draw(x, y)
-    #character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    character.clip_draw(frame * 100, 100 * turn, 100, 100, ch_x, ch_y)
     update_canvas()
     frame = (frame + 1) % 8
 
