ebd147162e4651005b69721a95ddb65cc4000502
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-09 18:15:45 +0900

    Drill06-1 최종 완성
    https://youtu.be/8bQvAXoBnxg
    커밋 횟수 : 2회
    2018180002강현석
    Drill06-1 최종 완성

--- Drill-06-1/curve.py
+++ Drill-06-1/curve.py
@@ -109,8 +109,8 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 prepare_turtle_canvas()
 
-
-draw_curve_4_points((-300, 200), (400, 350), (300, - 300), (-200, -200))
+while True:
+    draw_curve_4_points((-300, 200), (400, 350), (300, - 300), (-200, -200))
 
 
 turtle.done()
