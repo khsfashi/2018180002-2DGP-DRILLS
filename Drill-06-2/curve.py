51ef4d54f868242894fd7c154a8b0f4775f70b62
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-09 19:54:32 +0900

    Drill 06-2 최종 완성
    커밋 횟수 : 4회
    2018180002 강현석
    Drill 06-2 최종 완성
    https://youtu.be/qNsdx-2P7o8

--- Drill-06-2/curve.py
+++ Drill-06-2/curve.py
@@ -3,144 +3,6 @@ import random
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-def moving_curve_3_points(p1, p2, p3):
-    global x, y
-    global i
-    t = i / 100
-    x = (2*t**2-3*t+1) * p1[0] + (-4*t**2+4*t)*p2[0] + (2*t**2-t)*p3[0]
-    y = (2*t**2-3*t+1) * p1[1] + (-4*t**2+4*t)*p2[1] + (2*t**2-t)*p3[1]
-
-    if i>= 100:
-        i = 0
-    delay(0.01)
-    pass
-
-
-def moving_curve_4_points(p1, p2, p3, p4):
-    global x, y
-    global i
-    # draw p1-p2
-    for i in range(0, 50, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-
-
-    # draw p2-p3
-    for i in range(0, 100, 2):
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-
-
-
-    # draw p3-p4
-    for i in range(50, 100, 2):
-        t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-
-
-def moving_curve_10_points(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10):
-    global x,y
-    global i
-    global turn
-    # draw p1-p2
-    if turn == 0:
-        t = i / 100
-        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
-        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
-        if i >= 50:
-            i = 0
-            turn = 1
-
-    # draw p2-p3
-    if turn == 1:
-        t = i / 100
-        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
-        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
-        if i >= 100:
-            i = 50
-            turn = 2
-
-    # draw p3-p4
-    if turn == 2:
-        t = i / 100
-        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
-        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
-        if i >= 100:
-            i = 0
-            turn = 3
-
-    # draw p4-p5
-    if turn == 3:
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p4[0] + (-4 * t ** 2 + 4 * t) * p5[0] + (2 * t ** 2 - t) * p6[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p4[1] + (-4 * t ** 2 + 4 * t) * p5[1] + (2 * t ** 2 - t) * p6[1]
-        if i >= 50:
-            i = 0
-            turn = 4
-
-    # draw p5-p6
-    if turn == 4:
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
-                    -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
-        if i >= 100:
-            i = 50
-            turn = 5
-
-    # draw p6-p7
-    if turn == 5:
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p5[0] + (-4 * t ** 2 + 4 * t) * p6[0] + (2 * t ** 2 - t) * p7[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p5[1] + (-4 * t ** 2 + 4 * t) * p6[1] + (2 * t ** 2 - t) * p7[1]
-        if i >= 100:
-            i = 0
-            turn = 6
-
-    # draw p7-p8
-    if turn == 6:
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p7[0] + (-4 * t ** 2 + 4 * t) * p8[0] + (2 * t ** 2 - t) * p9[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p7[1] + (-4 * t ** 2 + 4 * t) * p8[1] + (2 * t ** 2 - t) * p9[1]
-        if i >= 50:
-            i = 0
-            turn = 7
-
-    # draw p8-p9
-    if turn == 7:
-        t = i / 100
-        x = ((-t ** 3 + 2 * t ** 2 - t) * p7[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[0] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p9[0] + (t ** 3 - t ** 2) * p10[0]) / 2
-        y = ((-t ** 3 + 2 * t ** 2 - t) * p7[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[1] + (
-                -3 * t ** 3 + 4 * t ** 2 + t) * p9[1] + (t ** 3 - t ** 2) * p10[1]) / 2
-        if i >= 100:
-            i = 50
-            turn = 8
-
-    # draw p9-p10
-    if turn == 8:
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p8[0] + (-4 * t ** 2 + 4 * t) * p9[0] + (2 * t ** 2 - t) * p10[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p8[1] + (-4 * t ** 2 + 4 * t) * p9[1] + (2 * t ** 2 - t) * p10[1]
-        if i >= 100:
-            i = 0
-            turn = 9
-
-    # draw p10-p1
-    if turn == 9:
-        t = i / 100
-        x = (2 * t ** 2 - 3 * t + 1) * p10[0] + (-4 * t ** 2 + 4 * t) * p1[0] + (2 * t ** 2 - t) * p2[0]
-        y = (2 * t ** 2 - 3 * t + 1) * p10[1] + (-4 * t ** 2 + 4 * t) * p1[1] + (2 * t ** 2 - t) * p2[1]
-        if i >= 50:
-            i = 0
-            turn = 0
-
-
-
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
@@ -148,29 +10,139 @@ character = load_image('animation_sheet.png')
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-i = 0
-turn = 0
 hide_cursor()
+turn = 0
+i = 0
+
+p1 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p2 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p3 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p4 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p5 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p6 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p7 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p8 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p9 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
+p10 = (random.randint(0, 1000 + 1), random.randint(0, 800 + 1))
 
 while running:
+
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+
+    if i <= 100 and turn == 0:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[0] + (t ** 3 - t ** 2) * p3[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p1[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p2[1] + (t ** 3 - t ** 2) * p3[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 1
+
+    if i <= 100 and turn == 1:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 2
+
+    if i <= 100 and turn == 2:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p2[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p2[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p3[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p4[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 3
+
+    if i <= 100 and turn == 3:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p3[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p5[0] + (t ** 3 - t ** 2) * p6[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p3[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p4[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p5[1] + (t ** 3 - t ** 2) * p6[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 4
+
+    if i <= 100 and turn == 4:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p4[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p6[0] + (t ** 3 - t ** 2) * p7[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p4[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p5[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p6[1] + (t ** 3 - t ** 2) * p7[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 5
+
+    if i <= 100 and turn == 5:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p5[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p7[0] + (t ** 3 - t ** 2) * p8[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p5[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p6[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p7[1] + (t ** 3 - t ** 2) * p8[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 6
+
+    if i <= 100 and turn == 6:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p6[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p8[0] + (t ** 3 - t ** 2) * p9[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p6[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p7[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p8[1] + (t ** 3 - t ** 2) * p9[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 7
+
+    if i <= 100 and turn == 7:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p7[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p9[0] + (t ** 3 - t ** 2) * p10[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p7[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p8[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p9[1] + (t ** 3 - t ** 2) * p10[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 8
+
+    if i <= 100 and turn == 8:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p8[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p10[0] + (t ** 3 - t ** 2) * p1[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p8[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p9[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p10[1] + (t ** 3 - t ** 2) * p1[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 9
+
+    if i <= 100 and turn == 9:
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p9[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[0] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[0] + (t ** 3 - t ** 2) * p2[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p9[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p10[1] + (
+                -3 * t ** 3 + 4 * t ** 2 + t) * p1[1] + (t ** 3 - t ** 2) * p2[1]) / 2
+        i += 1
+        if i >= 100:
+            i = 0
+            turn = 0
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
-    i += 1
-    moving_curve_10_points((random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)),
-                           (random.randint(0, 1000 + 1), random.randint(0, 800 + 1)))
-    delay(0.01)
-
+    delay(0.02)
 
 
 
