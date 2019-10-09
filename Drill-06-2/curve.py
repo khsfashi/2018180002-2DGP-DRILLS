83d23ff6f5e8fe3f3161d5a189e81544fb010faa
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-09 18:31:56 +0900

    Drill 06-2 시작
    06 - 2 시작

---
+++ Drill-06-2/curve.py
@@ -0,0 +1,61 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+def moving_curve_3_points(p1, p2, p3):
+    global x, y
+    global i
+    t = i / 100
+    x = (2*t**2-3*t+1) * p1[0] + (-4*t**2+4*t)*p2[0] + (2*t**2-t)*p3[0]
+    y = (2*t**2-3*t+1) * p1[1] + (-4*t**2+4*t)*p2[1] + (2*t**2-t)*p3[1]
+
+    if i>= 100:
+        i = 0
+    pass
+
+
+def moving_curve_4_points(p1, p2, p3, p4):
+    global x, y
+    global i
+    global character
+    # draw p1-p2
+    for i in range(0, 50, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
+        y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
+
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        t = i / 100
+        x = ((-t**3 + 2*t**2 - t)*p1[0] + (3*t**3 - 5*t**2 + 2)*p2[0] + (-3*t**3 + 4*t**2 + t)*p3[0] + (t**3 - t**2)*p4[0])/2
+        y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
+
+
+    # draw p3-p4
+    for i in range(50, 100, 2):
+        t = i / 100
+        x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
+        y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+frame = 0
+i = 0
+hide_cursor()
+
+while running:
+    clear_canvas()
+    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    moving_curve_3_points((-350, -100), (-50, 200), (150, -100))
+
+
+close_canvas()
