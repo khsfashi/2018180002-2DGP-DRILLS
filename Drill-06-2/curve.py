252bd55efa5b35d1e6506f1eb288996c44719de1
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-09 18:36:49 +0900

    Drill 06-2
    세 점 이동 구현
    커밋 2회

--- Drill-06-2/curve.py
+++ Drill-06-2/curve.py
@@ -12,6 +12,7 @@ def moving_curve_3_points(p1, p2, p3):
 
     if i>= 100:
         i = 0
+    delay(0.01)
     pass
 
 
@@ -25,6 +26,7 @@ def moving_curve_4_points(p1, p2, p3, p4):
         x = (2*t**2-3*t+1)*p1[0]+(-4*t**2+4*t)*p2[0]+(2*t**2-t)*p3[0]
         y = (2*t**2-3*t+1)*p1[1]+(-4*t**2+4*t)*p2[1]+(2*t**2-t)*p3[1]
 
+
     # draw p2-p3
     for i in range(0, 100, 2):
         t = i / 100
@@ -32,11 +34,13 @@ def moving_curve_4_points(p1, p2, p3, p4):
         y = ((-t**3 + 2*t**2 - t)*p1[1] + (3*t**3 - 5*t**2 + 2)*p2[1] + (-3*t**3 + 4*t**2 + t)*p3[1] + (t**3 - t**2)*p4[1])/2
 
 
+
     # draw p3-p4
     for i in range(50, 100, 2):
         t = i / 100
         x = (2*t**2-3*t+1)*p2[0]+(-4*t**2+4*t)*p3[0]+(2*t**2-t)*p4[0]
         y = (2*t**2-3*t+1)*p2[1]+(-4*t**2+4*t)*p3[1]+(2*t**2-t)*p4[1]
+
 
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -55,7 +59,9 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     update_canvas()
     frame = (frame + 1) % 8
-    moving_curve_3_points((-350, -100), (-50, 200), (150, -100))
+    i += 1
+    moving_curve_3_points((350, 100), (50, 200), (150, 100))
+
 
 
 close_canvas()
