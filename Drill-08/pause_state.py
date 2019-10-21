5b35406b3392215f6d021f73982ab17e555ff521
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-21 19:12:17 +0900

    Drill 08 - 2 최종완성
    2018180002 강현석
    Drill 08 - 1 커밋횟수 : 1회
    Drill 08 - 2 커밋횟수 : 1회
    Drill 08 총 커밋횟수 : 2회
    
    Drill 08 - 2 최종 완성
    Drill 08 - 1 최종 완성에 대한 코드는 이전 커밋을 참조
    
    유튜브 링크
    
    Drill 08 - 1
    https://youtu.be/6ji1dX7w1oA
    
    Drill 08 - 2
    https://youtu.be/S1vqOEJb4KQ

--- Drill-08/pause_state.py
+++ Drill-08/pause_state.py
@@ -1,7 +1,10 @@
 import game_framework
 from pico2d import *
+import main_state
 
 name = "PauseState"
+logo_time = 0.0
+pause_image = True
 
 def enter():
     global image
@@ -14,25 +17,42 @@ def exit():
 
 
 def update():
-    pass
+    global logo_time
+    global pause_image
+
+    if logo_time > 0.3:
+        logo_time = 0
+        if pause_image == True:
+            pause_image = False
+        else:
+            pause_image = True
+
+    delay(0.01)
+    logo_time += 0.01
 
 
 def draw():
     global image
     clear_canvas()
-    image.draw(400, 300)
+    main_state.grass.draw()
+    main_state.boy.draw()
+    if pause_image == True:
+        image.clip_draw(0, 0, 900, 900, 400, 300, 200, 200)
     update_canvas()
 
 
 
-
 def handle_events():
+    global logo_time
+    global pause_image
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
             game_framework.pop_state()
+            logo_time = 0.0
+            pause_image = True
 
 
 def pause(): pass
