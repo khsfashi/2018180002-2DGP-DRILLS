2af8585bc35e50c3812d1b399fbc425efa98783b
KHS_C106_PC04 <khsfashi@naver.com>
2019-10-21 18:53:26 +0900

    Drill 08 - 1 최종완료
    Drill 08 - 1 최종완료
    커밋횟수 : 1회
    2018180002 강현석

---
+++ Drill-08/pause_state.py
@@ -0,0 +1,41 @@
+import game_framework
+from pico2d import *
+
+name = "PauseState"
+
+def enter():
+    global image
+    image = load_image('pause.png')
+
+
+def exit():
+    global image
+    del(image)
+
+
+def update():
+    pass
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.pop_state()
+
+
+def pause(): pass
+
+
+def resume(): pass
